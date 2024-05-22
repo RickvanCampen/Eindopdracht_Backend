03d85bced4deaa38f105071f1a4e541e8a400a55
RickvanCampen <rickvancampen11@gmail.com>
2024-05-22 14:05:16 +0200

    First commit

---
+++ src/main/java/com/example/eindopdracht_backend_ipmroved/EindopdrachtBackendIpmrovedApplication.java
@@ -0,0 +1,13 @@
+package com.example.eindopdracht_backend_ipmroved;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindopdrachtBackendIpmrovedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EindopdrachtBackendIpmrovedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eindopdracht_backend_ipmroved/EindopdrachtBackendIpmrovedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eindopdracht_backend_ipmroved;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindopdrachtBackendIpmrovedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
